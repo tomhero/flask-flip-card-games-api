0c6c5cb7b6fc90e7beda79db8496909c376b1933
Chayut <chayut.191@gmail.com>
2019-12-28 12:15:37 +0700

    Add CORS

--- services/api/project/__init__.py
+++ services/api/project/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from pymongo import MongoClient
 from project.model.high_score import HighScore
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object("project.config.Config")
 
 client = MongoClient(app.config["PYMONGO_DATABASE_URI"])
