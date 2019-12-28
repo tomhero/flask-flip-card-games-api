a9ff3c1b3d00d5d59c34ee02264fdb9c0652fc14
Chayut <chayut.191@gmail.com>
2019-12-28 16:32:31 +0700

    Test for Certs

--- services/api/project/__init__.py
+++ services/api/project/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_talisman import Talisman
 from pymongo import MongoClient
 from project.model.high_score import HighScore
 
 app = Flask(__name__)
 CORS(app)
+Talisman(app)
 app.config.from_object("project.config.Config")
 
 client = MongoClient(app.config["PYMONGO_DATABASE_URI"])
